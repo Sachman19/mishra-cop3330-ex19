5f14faf46e0fec151bff43d7505ab14efc7b467c
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 07:35:11 -0400

    V1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Please enter your weight in lbs. ");
+        double weight = input.nextDouble();
+
+        System.out.print("Please enter your height in inches. ");
+        double height = input.nextDouble();
+
+        double bmi = (weight / (height * height)) * 703;
+        System.out.printf("Your BMI is %.1f.%n", bmi);
+
+        if(bmi < 18.5){
+            System.out.print("You are underweight. Please see a doctor.");
+        }
+        else if(bmi <= 25){
+            System.out.print("You are within a healthy weight range.");
+        }
+        else{
+            System.out.print("You are overweight. Please see a doctor.");
+        }
     }
 }
