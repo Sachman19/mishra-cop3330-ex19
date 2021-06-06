ac5d09c2e1eab7988415f2b32d4eab2fa56919c0
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 07:30:07 -0400

    S

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 19 Solution
+ *  Copyright 2021 Sachin Mishra
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
